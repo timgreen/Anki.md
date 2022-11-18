ba73935b0dcfcbdf729f190d7c537f21f88ef853
Tim Green <iamtimgreen@gmail.com>
2022-11-18 18:49:44 +1100

    Create models defined in the frontmatter.

--- packages/core/src/actions/ankiconnect.ts
+++ packages/core/src/actions/ankiconnect.ts
@@ -2,15 +2,6 @@ import { invoke, ModelTypes, NoteTypes } from "@autoanki/anki-connect";
 
 import { IDeck, INoteType } from "../model";
 
-function toNewNotes(deck: IDeck): NoteTypes.NewNote[] {
-  return deck.notes.map((n) => ({
-    deckName: deck.deckName,
-    modelName: n.modelName,
-    fields: n.values,
-    tags: n.tags,
-  }));
-}
-
 function createDefaultCardTemplateForNoteType(
   noteType: INoteType,
 ): ModelTypes.CardTemplate {
@@ -39,35 +30,60 @@ export async function ankiConnectSync(
     },
   });
 
-  const modelNames = await invoke({
+  const existingModelNames = new Set<string>();
+  for (const modelName in await invoke({
     action: "modelNamesAndIds",
     version: 6,
     request: undefined,
-  });
+  })) {
+    existingModelNames.add(modelName);
+  }
 
-  await Promise.all(
-    deck.notes.map((n) => {
-      if (!modelNames[n.modelName]) {
-        return invoke({
-          action: "createModel",
-          version: 6,
-          request: {
-            modelName: n.modelName,
-            inOrderFields: n.inOrderFields,
-            isCloze: false,
-            cardTemplates: [createDefaultCardTemplateForNoteType(n)],
-            css: "",
-          },
-        });
-      }
-    }),
-  );
+  // createModel defined in the frontmatter
+  const models = deck.frontmatterConfig?.models || {};
+  for (const modelName in models) {
+    if (!existingModelNames.has(modelName)) {
+      await invoke({
+        action: "createModel",
+        version: 6,
+        request: {
+          ...models[modelName],
+          isCloze: false,
+          modelName: modelName,
+        },
+      });
+      existingModelNames.add(modelName);
+    }
+  }
+
+  // createModel for notes
+  for (const note of deck.notes) {
+    if (!existingModelNames.has(note.modelName)) {
+      await invoke({
+        action: "createModel",
+        version: 6,
+        request: {
+          modelName: note.modelName,
+          inOrderFields: note.inOrderFields,
+          isCloze: false,
+          cardTemplates: [createDefaultCardTemplateForNoteType(note)],
+          css: "",
+        },
+      });
+      existingModelNames.add(note.modelName);
+    }
+  }
 
   return invoke({
     action: "addNotes",
     version: 6,
     request: {
-      notes: toNewNotes(deck),
+      notes: deck.notes.map((n) => ({
+        deckName: deck.deckName,
+        modelName: n.modelName,
+        fields: n.values,
+        tags: n.tags,
+      })),
     },
   });
 }
