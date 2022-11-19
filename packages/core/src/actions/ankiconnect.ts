f0716a5ad2aad02c3c8d8db8f4632d6b4e587ef9
Tim Green <iamtimgreen@gmail.com>
2022-11-20 00:59:57 +1100

    [core] store medias.

--- packages/core/src/actions/ankiconnect.ts
+++ packages/core/src/actions/ankiconnect.ts
@@ -74,6 +74,22 @@ export async function ankiConnectSync(
     }
   }
 
+  // store medias
+  for (const note of deck.notes) {
+    for (const mediaName in note.medias) {
+      const media = note.medias[mediaName];
+      await invoke({
+        action: "storeMediaFile",
+        version: 6,
+        request: {
+          filename: mediaName,
+          path: media.absPath,
+          url: media.url,
+        },
+      });
+    }
+  }
+
   return invoke({
     action: "addNotes",
     version: 6,
