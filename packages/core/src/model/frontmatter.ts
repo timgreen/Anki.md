2b1a799fb9a822a53bd8f32ff096e81e97d8645c
Tim Green <iamtimgreen@gmail.com>
2022-11-26 23:21:31 +1100

    feat: not allow overriding default models.
    close #41
    
    docs: more example & pattern in json schema.

--- packages/core/src/model/frontmatter.ts
+++ packages/core/src/model/frontmatter.ts
@@ -1,27 +1,40 @@
-import { ModelName } from "@autoanki/anki-connect/dist/model";
 import { NoteModel } from "./model";
+
+/**
+ * The name of the note model.
+ *
+ * Override the following default NoteModels are not allowed:
+ * - "Basic"
+ * - "Basic (and reversed card)"
+ * - "Basic (optional reversed card)"
+ * - "Basic (type in the answer)"
+ * - "Cloze"
+ *
+ * @pattern ^(?!(Basic|Basic \(and reversed card\)|Basic \(optional reversed card\)|Basic \(type in the answer\)|Cloze)$)\w.*
+ */
+type ModelName = string;
 
 export interface IFrontmatterConfig {
   /**
    * The container deck for the notes in the file.
    *
-   * - Default to "Default" if not specified.
-   * - Use "::" to create nested decks. e.g. "a::b::c".
+   * @default "Default"
+   *
+   * @example
+   *   "deckA"
+   * @example
+   *   "a::b::c"
    */
   deckName?: string;
   /**
    * Define the custom NoteModel for the notes in the file.
-   *
-   * Override the following default NoteModels are not allowed:
-   * - "Basic"
-   * - "Basic (and reversed card)"
-   * - "Basic (optional reversed card)"
-   * - "Basic (type in the answer)"
-   * - "Cloze"
    */
   models?: Record<ModelName, NoteModel>;
   /**
    * Global tags for the notes in the deck.
+   *
+   * @example
+   *   "global-tag-1"
    */
   tags?: string[];
 }
