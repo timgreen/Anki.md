f7b6d874e28fc6f4373a654f4c6ce8dd58a3cf6f
Tim Green <iamtimgreen@gmail.com>
2023-01-24 06:09:16 +0000

    chore(deps): bump @oclif/core from 1.26.1 to 2.0.3

--- packages/cli/src/commands/rmids.ts
+++ packages/cli/src/commands/rmids.ts
@@ -1,35 +1,35 @@
 import { Parser, updateNoteId } from "@anki.md/core";
-import { Command } from "@oclif/core";
+import { Command, Args } from "@oclif/core";
 import * as fs from "fs";
 import * as path from "path";
 
 export default class RmIds extends Command {
-  static description = "Remove the note IDs.";
+	static description = "Remove the note IDs.";
 
-  static strict = false;
+	static strict = false;
 
-  static examples = [
-    "<%= config.bin %> <%= command.id %> deckA.md",
-    "<%= config.bin %> <%= command.id %> deckA.md deckB.md",
-  ];
+	static examples = [
+		"<%= config.bin %> <%= command.id %> deckA.md",
+		"<%= config.bin %> <%= command.id %> deckA.md deckB.md",
+	];
 
-  static args = [
-    { name: "files", required: true, description: "Markdown files" },
-  ];
+	static args = {
+		files: Args.file({ required: true, description: "Markdown files" }),
+	};
 
-  public async run(): Promise<void> {
-    const { argv, flags } = await this.parse(RmIds);
-    const parser = new Parser({});
+	public async run(): Promise<void> {
+		const { argv, flags } = await this.parse(RmIds);
+		const parser = new Parser({});
 
-    for (const input of argv) {
-      this.log(input);
-      const content = String(fs.readFileSync(input));
-      const deck = await parser.parse(content, path.dirname(input));
-      deck.notes.forEach((n) => (n.noteId = undefined));
-      const updatedContent = updateNoteId(content, deck.notes);
-      if (content != updatedContent) {
-        fs.writeFileSync(input, updatedContent);
-      }
-    }
-  }
+		for (const input of (argv as string[])) {
+			this.log(input);
+			const content = String(fs.readFileSync(input));
+			const deck = await parser.parse(content, path.dirname(input));
+			deck.notes.forEach((n) => (n.noteId = undefined));
+			const updatedContent = updateNoteId(content, deck.notes);
+			if (content != updatedContent) {
+				fs.writeFileSync(input, updatedContent);
+			}
+		}
+	}
 }
