900f4f80dd8905707cf6a0051c3d2a510bdd7aa2
Tim Green <iamtimgreen@gmail.com>
2023-01-24 06:17:43 +0000

    chore: fix lint.

--- packages/cli/src/commands/rmids.ts
+++ packages/cli/src/commands/rmids.ts
@@ -4,32 +4,32 @@ import * as fs from "fs";
 import * as path from "path";
 
 export default class RmIds extends Command {
-	static description = "Remove the note IDs.";
+  static description = "Remove the note IDs.";
 
-	static strict = false;
+  static strict = false;
 
-	static examples = [
-		"<%= config.bin %> <%= command.id %> deckA.md",
-		"<%= config.bin %> <%= command.id %> deckA.md deckB.md",
-	];
+  static examples = [
+    "<%= config.bin %> <%= command.id %> deckA.md",
+    "<%= config.bin %> <%= command.id %> deckA.md deckB.md",
+  ];
 
-	static args = {
-		files: Args.file({ required: true, description: "Markdown files" }),
-	};
+  static args = {
+    files: Args.file({ required: true, description: "Markdown files" }),
+  };
 
-	public async run(): Promise<void> {
-		const { argv, flags } = await this.parse(RmIds);
-		const parser = new Parser({});
+  public async run(): Promise<void> {
+    const { argv, flags } = await this.parse(RmIds);
+    const parser = new Parser({});
 
-		for (const input of (argv as string[])) {
-			this.log(input);
-			const content = String(fs.readFileSync(input));
-			const deck = await parser.parse(content, path.dirname(input));
-			deck.notes.forEach((n) => (n.noteId = undefined));
-			const updatedContent = updateNoteId(content, deck.notes);
-			if (content != updatedContent) {
-				fs.writeFileSync(input, updatedContent);
-			}
-		}
-	}
+    for (const input of argv as string[]) {
+      this.log(input);
+      const content = String(fs.readFileSync(input));
+      const deck = await parser.parse(content, path.dirname(input));
+      deck.notes.forEach((n) => (n.noteId = undefined));
+      const updatedContent = updateNoteId(content, deck.notes);
+      if (content != updatedContent) {
+        fs.writeFileSync(input, updatedContent);
+      }
+    }
+  }
 }
--- packages/cli/src/commands/sync.ts
+++ packages/cli/src/commands/sync.ts
@@ -6,81 +6,81 @@ import * as path from "path";
 import { isUri } from "valid-url";
 
 export default class Sync extends Command {
-	static description = "Sync to Anki Desktop via AnkiConnect.";
+  static description = "Sync to Anki Desktop via AnkiConnect.";
 
-	static strict = false;
+  static strict = false;
 
-	static examples = [
-		"<%= config.bin %> <%= command.id %> deckA.md",
-		"<%= config.bin %> <%= command.id %> deckA.md deckB.md",
-		"<%= config.bin %> <%= command.id %> deckA.md https://example.com/foo/deckB.md",
-	];
+  static examples = [
+    "<%= config.bin %> <%= command.id %> deckA.md",
+    "<%= config.bin %> <%= command.id %> deckA.md deckB.md",
+    "<%= config.bin %> <%= command.id %> deckA.md https://example.com/foo/deckB.md",
+  ];
 
-	static args = {
-		markdowns: Args.file({ required: true, description: "Markdown files" }),
-	};
+  static args = {
+    markdowns: Args.file({ required: true, description: "Markdown files" }),
+  };
 
-	static flags = {
-		math: Flags.string({
-			description:
-				"render math equations as SVG or use Anki native mathjax support.",
-			default: "native",
-			options: ["native", "svg"],
-			required: false,
-		}),
-		"update-model-templates": Flags.boolean({
-			description: "update the card templates for the existing note models.",
-		}),
-		"update-model-styling": Flags.boolean({
-			description: "update the styling for the existing note models.",
-		}),
-		"save-note-ids": Flags.boolean({
-			description:
-				"save the note IDs in markdown after sync. \nIt will be used to update note instead insert on next sync",
-			default: true,
-			required: false,
-			allowNo: true,
-			aliases: ["save-note-id"],
-		}),
-	};
+  static flags = {
+    math: Flags.string({
+      description:
+        "render math equations as SVG or use Anki native mathjax support.",
+      default: "native",
+      options: ["native", "svg"],
+      required: false,
+    }),
+    "update-model-templates": Flags.boolean({
+      description: "update the card templates for the existing note models.",
+    }),
+    "update-model-styling": Flags.boolean({
+      description: "update the styling for the existing note models.",
+    }),
+    "save-note-ids": Flags.boolean({
+      description:
+        "save the note IDs in markdown after sync. \nIt will be used to update note instead insert on next sync",
+      default: true,
+      required: false,
+      allowNo: true,
+      aliases: ["save-note-id"],
+    }),
+  };
 
-	public async run(): Promise<void> {
-		const { argv, flags } = await this.parse(Sync);
-		const parser = new Parser({
-			useSvgMathjax: flags.math === "svg",
-		});
+  public async run(): Promise<void> {
+    const { argv, flags } = await this.parse(Sync);
+    const parser = new Parser({
+      useSvgMathjax: flags.math === "svg",
+    });
 
-		for (const input of (argv as string[])) {
-			this.log(input);
-			const isRemote = isUri(input);
-			const content = isRemote
-				? await (await fetch(input)).text()
-				: String(fs.readFileSync(input));
-			const deck = await parser.parse(
-				content,
-				isRemote ? input : path.dirname(input)
-			);
-			const noteIds = await ankiConnectSync(deck, {
-				updateModelTemplates: flags["update-model-templates"],
-				updateModelStyling: flags["update-model-styling"],
-			});
+    for (const input of argv as string[]) {
+      this.log(input);
+      const isRemote = isUri(input);
+      const content = isRemote
+        ? await (await fetch(input)).text()
+        : String(fs.readFileSync(input));
+      const deck = await parser.parse(
+        content,
+        isRemote ? input : path.dirname(input),
+      );
+      const noteIds = await ankiConnectSync(deck, {
+        updateModelTemplates: flags["update-model-templates"],
+        updateModelStyling: flags["update-model-styling"],
+      });
 
-			if (flags["save-note-ids"]) {
-				if (isRemote) {
-					// TODO: warning about skip save note ids for remote files.
-				} else {
-					// TODO: check if the source if readonly.
-					noteIds.forEach((noteId, index) => {
-						if (noteId) {
-							deck.notes[index].noteId = noteId;
-						}
-					});
-					const updatedContent = updateNoteId(content, deck.notes);
-					if (content != updatedContent) {
-						fs.writeFileSync(input, updatedContent);
-					}
-				}
-			}
-		}
-	}
+      if (flags["save-note-ids"]) {
+        if (isRemote) {
+          // TODO: warning about skip save note ids for remote files.
+        } else {
+          // TODO: check if the source if readonly.
+          noteIds.forEach((noteId, index) => {
+            if (noteId) {
+              deck.notes[index].noteId = noteId;
+            }
+          });
+          const updatedContent = updateNoteId(content, deck.notes);
+          if (content != updatedContent) {
+            fs.writeFileSync(input, updatedContent);
+          }
+        }
+      }
+    }
+  }
 }
