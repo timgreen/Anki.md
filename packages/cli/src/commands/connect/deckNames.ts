eb74d2bd948b8cf83cb5857b16c9a89951437a5c
Tim Green <iamtimgreen@gmail.com>
2023-03-10 14:06:54 +1100

    feat: support json output for connect wrapper
    https://oclif.io/docs/json

--- packages/cli/src/commands/connect/deckNames.ts
+++ packages/cli/src/commands/connect/deckNames.ts
@@ -6,15 +6,18 @@ export default class DeckNames extends Command {
     "Gets the complete list of deck names for the current user.";
 
   static strict = true;
+  static enableJsonFlag = true;
 
   static examples = ["<%= config.bin %> <%= command.id %>"];
 
-  public async run(): Promise<void> {
+  public async run(): Promise<string[]> {
     const deckNames = await invoke({
       action: "deckNames",
       version: 6,
       request: undefined,
     });
     this.log(deckNames.join("\n"));
+
+    return deckNames;
   }
 }
--- packages/cli/src/commands/connect/getProfiles.ts
+++ packages/cli/src/commands/connect/getProfiles.ts
@@ -5,15 +5,18 @@ export default class GetProfiles extends Command {
   static description = "Retrieve the list of profiles.";
 
   static strict = true;
+  static enableJsonFlag = true;
 
   static examples = ["<%= config.bin %> <%= command.id %>"];
 
-  public async run(): Promise<void> {
+  public async run(): Promise<string[]> {
     const profiles = await invoke({
       action: "getProfiles",
       version: 6,
       request: undefined,
     });
     this.log(profiles.join("\n"));
+
+    return profiles;
   }
 }
